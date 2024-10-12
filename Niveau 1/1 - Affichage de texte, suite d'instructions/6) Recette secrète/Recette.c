06fe2370963c902669862f03b4e08b7a0bde8024
Anerti <168828334+Anerti@users.noreply.github.com>
2024-10-12 10:05:35 +0300

    Create Recette.c

---
+++ Niveau 1/1 - Affichage de texte, suite d'instructions/6) Recette secrète/Recette.c
@@ -0,0 +1,14 @@
+#include "robot.h"
+#include <stdio.h>
+
+int main()
+{
+   remplir(3);
+   transferer(3, 5);
+   remplir(3);
+   transferer(3, 5);
+   vider(5);
+   transferer(3, 5);
+   remplir(3);
+   transferer(3, 5);
+}
