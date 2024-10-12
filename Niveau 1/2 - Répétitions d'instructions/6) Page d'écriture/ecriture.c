8348a2195894e10c30618d26da32631c7bbd37aa
Anerti <168828334+Anerti@users.noreply.github.com>
2024-10-12 11:54:56 +0300

    C

---
+++ Niveau 1/2 - Répétitions d'instructions/6) Page d'écriture/ecriture.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   for (int i = 0; i < 30; i++)
+   {
+      printf("a_");
+   }
+   printf("\n");
+   for (int i = 0; i < 30; i++)
+   {
+      printf("b_");
+   }
+   printf("\n");
+   for (int i = 0; i < 30; i++)
+   {
+      printf("c_");
+   }
+   printf("\n");
+}
