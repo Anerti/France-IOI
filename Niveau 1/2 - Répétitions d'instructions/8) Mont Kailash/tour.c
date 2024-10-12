f26745bdde0c2ae6fd089517b9784ca9525a6337
Anerti <168828334+Anerti@users.noreply.github.com>
2024-10-12 12:04:56 +0300

    C

---
+++ Niveau 1/2 - Répétitions d'instructions/8) Mont Kailash/tour.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "robot.h"
+
+int main()
+{
+   for (int i = 0; i < 108; i++)
+   {
+      for (int j = 0; j < 13; j++)
+      {
+         haut();
+      }
+      for (int j = 0; j < 13; j++)
+      {
+         droite();
+      }
+      for (int j = 0; j < 13; j++)
+      {
+         bas();
+      }
+      for (int j = 0; j < 13; j++)
+      {
+         gauche();
+      }
+   }
+}
