7b311d822cd0731bf76f6c4b3df72154ce410186
Anerti <168828334+Anerti@users.noreply.github.com>
2024-10-16 16:43:23 +0300

    Python

---
+++ Niveau 1/	3 – Calculs et découverte des variables/4) L'algoréathlon/calcul.py
@@ -0,0 +1,11 @@
+Dnatation = 2
+Dcyclisme = 34
+Dcourse = 6
+Djour1 = Dnatation + Dcyclisme + Dcourse
+print(Djour1, end = "")
+print(" ", end = "")
+Djour2 = Djour1 + Dnatation + Dcyclisme + Dcourse
+print(Djour2, end = "")
+print(" ", end = "")
+Djour3 = Djour2 + Dnatation + Dcyclisme + Dcourse
+print(Djour3)
