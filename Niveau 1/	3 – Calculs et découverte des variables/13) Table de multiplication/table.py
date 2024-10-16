78cc07978d23db208ad07496e3585fe153b4f0d9
Anerti <168828334+Anerti@users.noreply.github.com>
2024-10-16 17:08:25 +0300

    Python

---
+++ Niveau 1/	3 – Calculs et découverte des variables/13) Table de multiplication/table.py
@@ -0,0 +1,9 @@
+affichage = 1
+temp = 1
+for loop in range(20):
+   for loop in range(20):
+      print(affichage, end = " ")
+      affichage = affichage + temp
+   print()
+   temp = temp + 1
+   affichage = temp
